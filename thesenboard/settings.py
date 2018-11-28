7f074395a0239032fa0c84a76a67922e14e3a73d
Alexis Michaltsis <alexis@michaltsis.net>
2018-11-28 14:21:51 +0100

    Use recommended full AppConfig name instead of simple app name

--- thesenboard/settings.py
+++ thesenboard/settings.py
@@ -28,7 +28,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'thesenboard',
-    'start',
+    'start.apps.StartConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
